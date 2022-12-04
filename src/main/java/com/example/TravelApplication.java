50d5b12603ccd3775c50dcb376e3dc14c5c44760
Peter <1185665292@qq.com>
2022-12-04 19:34:01 +0800

    first commit

--- src/main/java/com/example/TravelApplication.java
+++ src/main/java/com/example/TravelApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(TravelApplication.class, args);
     }
 
